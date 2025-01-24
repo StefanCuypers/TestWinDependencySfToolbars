d8be062ca6524db5a1964913e8dc89a448939332
Stefan Cuypers <stefan@comcu.eu>
2025-01-24 08:30:59 +0100

    Added Exception handling and environment information

--- TestWinDependencySfToolbars/TestWinDependencySfToolbars/App.xaml.cs
+++ TestWinDependencySfToolbars/TestWinDependencySfToolbars/App.xaml.cs
@@ -9,6 +9,15 @@ namespace TestWinDependencySfToolbars
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+        }
+
+        private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
     }
 
 }
